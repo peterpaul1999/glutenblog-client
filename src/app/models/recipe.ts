f13768910fc5ae4cc72b2646067edddaec05229a
Peter Paul <peterpaul1999@gmx.de>
2017-09-03 20:15:54 +0200

    update recipe class

--- src/app/models/recipe.ts
+++ src/app/models/recipe.ts
@@ -2,6 +2,13 @@ export class Recipe {
 
     public _id: string
     public name: string 
+    public numberOfServings: string
+    public description: string
+    public preperation: string
+    public img_small: string
+    public images: [string]
+    public ingredients: [string]
+    public categories: [string]
     public quote: string
 
     constructor() {
